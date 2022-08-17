8aa30ad98f794a37c52cf4050089604b783634b6
“Dmitriy” <colnce1-1@ya.ru>
2022-08-17 19:30:57 +0300

    filled with random floating point

---
+++ task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+Console.WriteLine("Введите число строк m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов n");
+int n = Convert.ToInt32(Console.ReadLine());
+Random rand = new Random();
+double[,] array = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+   for (int j = 0; j < n; j++)
+   {
+      array[i, j] = new Random().Next(-10, 10) + rand.NextDouble();
+      // Console.WriteLine($"Abs({array}) = {Math.Abs(array[i, j])}"); 
+       Console.Write(Math.Round(array[i, j], 1) + " ");  // 1 -один знак после запятой
+   }
+   Console.WriteLine("");
+}
